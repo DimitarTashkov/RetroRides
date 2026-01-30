253dc766b824e4665c1f2f70b93f58bd33d30376
DimitarTashkov <mitkotashkov1979@gmail.com>
2026-01-30 12:53:08 +0200

    Created a form handling adding and editing of exhibitions

--- RetroRides/Forms/AddEditExhibit.Designer.cs
+++ RetroRides/Forms/AddEditExhibit.Designer.cs
@@ -29,7 +29,185 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddEditExhibit));
+            btnBack = new Button();
+            btnSave = new Button();
+            pictureBox1 = new PictureBox();
+            pbImage = new Button();
+            txtDescription = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            txtMake = new TextBox();
+            label1 = new Label();
+            txtModel = new TextBox();
+            cmbType = new ComboBox();
+            numYear = new NumericUpDown();
+            label2 = new Label();
+            label6 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numYear).BeginInit();
             SuspendLayout();
+            // 
+            // btnBack
+            // 
+            btnBack.BackColor = Color.Orange;
+            btnBack.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBack.ForeColor = SystemColors.ControlText;
+            btnBack.Location = new Point(456, 529);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(100, 40);
+            btnBack.TabIndex = 41;
+            btnBack.Text = "Back";
+            btnBack.UseVisualStyleBackColor = false;
+            btnBack.Click += btnBack_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.Lime;
+            btnSave.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSave.Location = new Point(325, 529);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(100, 40);
+            btnSave.TabIndex = 39;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(689, 88);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(200, 198);
+            pictureBox1.TabIndex = 37;
+            pictureBox1.TabStop = false;
+            // 
+            // pbImage
+            // 
+            pbImage.BackColor = SystemColors.ActiveBorder;
+            pbImage.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            pbImage.ImeMode = ImeMode.NoControl;
+            pbImage.Location = new Point(710, 297);
+            pbImage.Margin = new Padding(3, 4, 3, 4);
+            pbImage.Name = "pbImage";
+            pbImage.Size = new Size(154, 34);
+            pbImage.TabIndex = 36;
+            pbImage.Text = "Upload image";
+            pbImage.UseVisualStyleBackColor = false;
+            pbImage.Click += PbImage_Click;
+            // 
+            // txtDescription
+            // 
+            txtDescription.Location = new Point(239, 400);
+            txtDescription.Multiline = true;
+            txtDescription.Name = "txtDescription";
+            txtDescription.Size = new Size(405, 91);
+            txtDescription.TabIndex = 35;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(266, 357);
+            label5.Name = "label5";
+            label5.Size = new Size(120, 28);
+            label5.TabIndex = 34;
+            label5.Text = "Description:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(279, 237);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 28);
+            label4.TabIndex = 32;
+            label4.Text = "Type:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(279, 176);
+            label3.Name = "label3";
+            label3.Size = new Size(75, 28);
+            label3.TabIndex = 30;
+            label3.Text = "Model:";
+            // 
+            // txtMake
+            // 
+            txtMake.Location = new Point(239, 136);
+            txtMake.Name = "txtMake";
+            txtMake.Size = new Size(149, 27);
+            txtMake.TabIndex = 29;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(279, 105);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 28);
+            label1.TabIndex = 28;
+            label1.Text = "Make:";
+            // 
+            // txtModel
+            // 
+            txtModel.Location = new Point(237, 207);
+            txtModel.Name = "txtModel";
+            txtModel.Size = new Size(149, 27);
+            txtModel.TabIndex = 42;
+            // 
+            // cmbType
+            // 
+            cmbType.FormattingEnabled = true;
+            cmbType.Items.AddRange(new object[] { "Car", "Motorcycle" });
+            cmbType.Location = new Point(237, 268);
+            cmbType.Name = "cmbType";
+            cmbType.Size = new Size(151, 28);
+            cmbType.TabIndex = 43;
+            // 
+            // numYear
+            // 
+            numYear.Location = new Point(239, 327);
+            numYear.Maximum = new decimal(new int[] { 2026, 0, 0, 0 });
+            numYear.Minimum = new decimal(new int[] { 1900, 0, 0, 0 });
+            numYear.Name = "numYear";
+            numYear.Size = new Size(150, 27);
+            numYear.TabIndex = 44;
+            numYear.Value = new decimal(new int[] { 1900, 0, 0, 0 });
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(279, 299);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 28);
+            label2.TabIndex = 45;
+            label2.Text = "Year:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(355, 49);
+            label6.Name = "label6";
+            label6.Size = new Size(265, 38);
+            label6.TabIndex = 46;
+            label6.Text = "Add or edit exhibit";
             // 
             // AddEditExhibit
             // 
@@ -38,13 +216,48 @@
             BackgroundImage = Properties.Resources.background;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(914, 600);
+            Controls.Add(label6);
+            Controls.Add(label2);
+            Controls.Add(numYear);
+            Controls.Add(cmbType);
+            Controls.Add(txtModel);
+            Controls.Add(btnBack);
+            Controls.Add(btnSave);
+            Controls.Add(pictureBox1);
+            Controls.Add(pbImage);
+            Controls.Add(txtDescription);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(txtMake);
+            Controls.Add(label1);
             DoubleBuffered = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "AddEditExhibit";
             Text = "AddEditExhibit";
+            Load += AddEditExhibit_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numYear).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnBack;
+        private Button btnSave;
+        private PictureBox pictureBox1;
+        private Button pbImage;
+        private TextBox txtDescription;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private TextBox txtMake;
+        private Label label1;
+        private TextBox txtModel;
+        private ComboBox cmbType;
+        private NumericUpDown numYear;
+        private Label label2;
+        private Label label6;
     }
 }
--- RetroRides/Forms/AddEditExhibit.cs
+++ RetroRides/Forms/AddEditExhibit.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RetroRides.Models;
+using RetroRides.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,153 @@ namespace RetroRides.Forms
 {
     public partial class AddEditExhibit : Form
     {
-        public AddEditExhibit()
+        private readonly IExhibitService _service;
+        private Exhibit _exhibit; // Текущият обект (ако е null = нов)
+        private string _selectedImagePath = null; // Пазим пътя до новата снимка
+        public AddEditExhibit(IExhibitService exhibitService, Exhibit exhibit)
         {
             InitializeComponent();
+            _service = exhibitService;
+            _exhibit = exhibit; 
+        }
+
+        private void AddEditExhibit_Load(object sender, EventArgs e)
+        {
+            SetupUI();
+        }
+        private void SetupUI()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+
+            // Проверка дали редактираме или създаваме
+            if (_exhibit != null)
+            {
+                this.Text = "Edit Vehicle";
+                FillForm();
+            }
+            else
+            {
+                this.Text = "Add New Vehicle";
+                _exhibit = new Exhibit(); // Инициализираме празен
+            }
+        }
+
+        private void FillForm()
+        {
+            txtMake.Text = _exhibit.Make;
+            txtModel.Text = _exhibit.Model;
+            txtDescription.Text = _exhibit.Description;
+
+            if (!string.IsNullOrEmpty(_exhibit.Type))
+                cmbType.SelectedItem = _exhibit.Type;
+
+            if (_exhibit.Year > 0)
+                numYear.Value = _exhibit.Year;
+
+            // Зареждане на снимка
+            if (!string.IsNullOrEmpty(_exhibit.ImagePath) && File.Exists(_exhibit.ImagePath))
+            {
+                try
+                {
+                    pbImage.Image = Image.FromFile(_exhibit.ImagePath);
+                    _selectedImagePath = _exhibit.ImagePath;
+                }
+                catch { pbImage.BackColor = Color.Gray; }
+            }
+        }
+
+        // === ИЗБОР НА СНИМКА (Логика от Prisma ManageProducts) ===
+        private void PbImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                ofd.Title = "Select Vehicle Image";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    // Показваме веднага
+                    pbImage.Image = Image.FromFile(ofd.FileName);
+                    // Пазим пътя за по-късно (при Save)
+                    _selectedImagePath = ofd.FileName;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // 1. Валидация
+            if (string.IsNullOrWhiteSpace(txtMake.Text) || string.IsNullOrWhiteSpace(txtModel.Text))
+            {
+                MessageBox.Show("Make and Model are required!", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (cmbType.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a Type (Car/Motorcycle)!", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 2. Обработка на снимката (Копиране в PhotosStorage - Логика от Prisma)
+            string finalImagePath = _selectedImagePath;
+
+            if (_selectedImagePath != null && !_selectedImagePath.Contains("PhotosStorage"))
+            {
+                // Създаваме папка, ако я няма
+                string storageFolder = Path.Combine(Application.StartupPath, "PhotosStorage");
+                if (!Directory.Exists(storageFolder)) Directory.CreateDirectory(storageFolder);
+
+                // Генерираме уникално име
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(_selectedImagePath);
+                string destPath = Path.Combine(storageFolder, fileName);
+
+                // Копираме
+                try
+                {
+                    File.Copy(_selectedImagePath, destPath, true);
+                    finalImagePath = destPath;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error copying image: " + ex.Message);
+                    return;
+                }
+            }
+
+            // 3. Запис в обекта
+            _exhibit.Make = txtMake.Text;
+            _exhibit.Model = txtModel.Text;
+            _exhibit.Type = cmbType.SelectedItem.ToString();
+            _exhibit.Year = (int)numYear.Value;
+            _exhibit.Description = txtDescription.Text;
+            _exhibit.ImagePath = finalImagePath;
+            _exhibit.IsActive = true;
+
+            // 4. Извикване на Сървиса
+            try
+            {
+                if (_exhibit.Id == Guid.Empty)
+                {
+                    _service.AddExhibit(_exhibit);
+                }
+                else
+                {
+                    _service.UpdateExhibit(_exhibit);
+                }
+
+                MessageBox.Show("Vehicle saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Program.SwitchMainForm(new ManageExhibits(_service));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving data: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            Program.SwitchMainForm(new ManageExhibits(_service));
         }
     }
 }
